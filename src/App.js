e16e83d0360517d598d47330f4e3a9900393b7e4
Daniel <daniel.mutembei@student.moringaschool.com>
2025-01-29 09:01:40 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+  <div>
+      <h1>Hi, I'm Your Name</h1>
+      <img src="public/images.png" alt="My photo" />
+      <h2>About Me</h2>
+      <p>Your biography text here.</p>
+      <a href="https://github.com/yourusername">GitHub</a>
+      <a href="https://linkedin.com/in/yourusername">LinkedIn</a>
+  </div>
+  )
 }
 
 export default App;
